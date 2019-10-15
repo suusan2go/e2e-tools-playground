bd1d1d4ab06e50e03948ba08d0e8286cc8676c5f
suusan2go <ksuzuki180@gmail.com>
2019-10-15 17:21:27 +0900

    save screnshot to artifacts

--- codeceptjs/codeceptjs_test.js
+++ codeceptjs/codeceptjs_test.js
@@ -5,4 +5,8 @@ Scenario("test something", I => {
   I.fillField("name", "suusan2go");
   I.click("Submit");
   I.see("Thank you, suusan2go!");
+  const random = Math.random()
+    .toString(36)
+    .slice(-8);
+  I.saveScreenshot("./screenshots/" + random + ".png");
 });
